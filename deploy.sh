92b68e45bbacf5ad5d40f33b9015083de9480623
Daniel <daniel.gaffarov@mail.ru>
2026-08-02 23:27:48 +0300

    commit2

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Переходим в папку проекта
+cd /root/parce_bot
+
+# Проверяем, есть ли новые коммиты на GitHub
+git fetch origin main
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse origin/main)
+
+if [ "$LOCAL" != "$REMOTE" ]; then
+    echo "Обнаружены изменения, обновляем проект..."
+    git pull origin main
+
+    # Обновляем образы и запускаем контейнеры через Docker Compose
+    docker compose pull
+    docker compose up -d --build
+
+    echo "Деплой успешно завершен!"
+else
+    echo "Обновлений нет."
+fi
